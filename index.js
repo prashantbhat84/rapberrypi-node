c7d6e1dabc98c4229daf883f0b5d0973559fa476
Prashant Bhat <prashantbhat84@gmail.com>
2021-03-10 14:36:41 +0530

    update code with api call

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(cors({ origin: "*" }));
 
@@ -8,7 +9,11 @@ app.use(express.json());
 
 const port = 3000;
 
-
+axios.post("https://rtcbackend.gariyasi.org", {
+    "id": "2"
+}).then(res => {
+    console.log(res.data)
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
