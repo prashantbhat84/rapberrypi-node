8dffad46eca26273f6bf4540d36656d18c63726e
Prashant Bhat <prashantbhat84@gmail.com>
2021-08-13 12:59:15 +0530

    modified raspberry pi code

--- index.js
+++ index.js
@@ -2,104 +2,101 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
-const serial= require("serialport");
+const serial = require("serialport");
 const ByteLength = require('@serialport/parser-byte-length');
-const dotenv = require('dotenv')
-dotenv.config({ path: "./config/config.env" });
-console.log(process.env.aggId);
-
-;
-//serial.flush();
-app.use(cors({ origin: "*" }));
-
-app.use(express.json());
-//const portName= '/dev/ttyAMA1';
-const portName= '/dev/ttyAMA2';
-let sp,sp1;
-try{
- sp= new serial(portName,{
-baudRate:115200,
-
-});
-const options={
-length:32
-}
-sp1=sp.pipe(new ByteLength(options))
-}catch(e){
-  console.log(e);
-}
-//setTimeout(async () => {
-  //  let ports = await serial.list();
-    //console.log(ports);
-//},0)
-/*for(let i=0; i<10;i++){
-let buff= Buffer.alloc(16,3);
-
-sp1.write(buff.write(3));
-}*/
-//sp.write("gariyasi");
-/*sp.on('open',()=>{
-console.log('port open');
-});*/
-/*sp.open(()=>{
-console.log("open");
-
-}*/
-//sp1.on('data',console.log);
-//sp.write("hello");
-
-sp1.on('data',(data)=>{
-console.log({data})
-const val= JSON.stringify(data);
-const  val1=(JSON.parse( val));
-console.log({val1});
-const val2=[];
-val1.data.forEach(item=>{
-  val2.push(item.toString(16))
-});
-//const send= String.fromCharCode(val1.data[5]);
-const send= val1.data[5].toString();
-const finaldata= send +"\r"+"\n";
-sp.write(send);
-sp.write("\r\n");
-
-//sp.write("\r\n");
-//rx=send.toString();
-// sp1.write(send);
-//console.log({val2});
-const macid= val2.slice(0,5);
-const boxid=val1.data.slice(5,14);
-const acctualdata= val2.slice(14,32);
-//console.log(String.fromCharCode(val1.data[13]));
-const val3= val1.data.slice(13,31);
-let  str="",str1="";
-let id1=""
-boxid.forEach(item=>{
-const id= item.toString(16);
-id1=id1 + id;
-});
-id1=+id1;
-val3.map(item=>{str=str +(String.fromCharCode(item))});
-console.log(str,id1);
-//console.log({macid,boxid,acctualdata});
-//val1.data.forEach(item=>{
-  //console.log(String.fromCharCode(item));
-//  console.log(item);
-//});
-});
-
-
-
+const os = require('os');
+const url = "https://tranquil-cove-09766.herokuapp.com";
+const timestamp = [];
 const port1 = 3000;
+const network = os.networkInterfaces();
+console.log(network)
+const id = (network['eth0'][0].mac);
+let aggid = id.split(":");
+let temp = "";
+aggid.forEach(item => {
+  temp = temp + item
+});
 
-/*axios.post("https://rtcbackend.gariyasi.org", {
-    "id": "pragati"
-}).then(res => {
-    console.log(res.data)
-})*/
+setInterval(() => {
+  console.log("aggregtor status send")
+  const server = `${url}/api/v1/aggregator/updateaggregator`
+  const internet = checkInternetConnection();
+  if (internet) {
+    axios.post(server, {
+      body: {
+        id: JSON.stringify(temp)
+      },
+
+    }).then(result => {
+      console.log(result.data)
+    }).catch(e => {
+      console.log(e.message)
+    })
+  }
+}, 10000)
+function createTimestampAndAggregator() {
+  const date = new Date();
+  timestamp.push(date.getUTCHours());
+  timestamp.push(date.getUTCMinutes());
+  timestamp.push(date.getUTCSeconds());
+  timestamp.push(date.getUTCMilliseconds());
+  timestamp.push(temp)
+  return timestamp;
+}
+function checkInternetConnection() {
+  const id = (network['wlan0']);
+  if (id) {
+    return true;
+  } else {
+    return false
+  }
+}
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+const portName = '/dev/ttyAMA2';
+let sp, sp1;
+sp = new serial(portName, {
+  baudRate: 115200,
+});
+const options = {
+  length: 32
+}
+sp1 = sp.pipe(new ByteLength(options))
+
+
+
+sp1.on('data', (data) => {
+  console.time("Data");
+  let val = JSON.stringify(data);
+  val = (JSON.parse(val));
+  const time = createTimestampAndAggregator();
+  let finaldata = [...val1.data, ...time]
+  const internet = checkInternetConnection();
+  if (internet) {
+    const server = `${url}/api/v1/aggregator/updateAggregatorAndBox`
+    axios.post(server, {
+      body: JSON.stringify(finaldata),
+
+    }).then(res => {
+      console.log(res.data)
+    })
+  }
+  console.timeEnd("Data")
+  // const send = val1.data[5].toString();
+  // const finaldata = send + "\r" + "\n";
+  // sp.write(send);
+  // sp.write("\r\n");
+});
+
+
+
+
+
+
 
 app.listen(port1, () => {
-    console.log(`Server is running on port ${port1}`)
+  console.log(`Server is running on port ${port1}`)
 })
 
 
--- rx.js
+++ rx.js
@@ -2,36 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
-const serial= require("serialport");
+const serial = require("serialport");
 const ByteLength = require('@serialport/parser-byte-length');
 const Readline = require('@serialport/parser-readline');
-const storage=[];
+const storage = [];
 ;
 //serial.flush();
 app.use(cors({ origin: "*" }));
 
 app.use(express.json());
-const portName= '/dev/ttyAMA1';
+const portName = '/dev/ttyAMA1';
+// let ports = await serial.list();
+// console.log(ports);
+//serial.flush();
+let sp, sp1;
+try {
+    sp = new serial(portName, {
+        baudRate: 115200,
 
-let sp,sp1;
-try{
- sp= new serial(portName,{
-baudRate:115200,
-
-});
-const options={
-length:32
+    });
+    const options = {
+        length: 32
+    }
+    sp1 = sp.pipe(new ByteLength(options))
+} catch (e) {
+    console.log(e);
 }
-sp1=sp.pipe(new ByteLength(options))
-}catch(e){
-  console.log(e);
-}
-let i=10;
+let i = 10;
 
-i=i.toString();
-for(let j=0;j<10;j++){
-sp.write(i);
-sp.write("\r\n");
+i = i.toString();
+for (let j = 0; j < 10; j++) {
+    sp.write(i);
+    sp.write("\r\n");
 }
 
 
