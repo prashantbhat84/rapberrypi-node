ae7cf7886f47353061f7bf264cf8db5de8b2943f
Prashant Bhat <prashantbhat84@gmail.com>
2021-11-15 11:40:59 +0530

    test

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const url = "https://tranquil-cove-09766.herokuapp.com";
 let timestamp = [];
 const port1 = 3000;
 const network = os.networkInterfaces();
+const crypto= require('crypto-js')
 
-const id = (network['eth0'][0].mac);
+while(!checkInternetConnection()) {
+  console.log("No internet connection");
+}
+
+const id = (network['wlan0'][0].mac);
+
+
 let aggid = id.split(":");
 let temp = "";
 aggid.forEach(item => {
@@ -36,10 +43,13 @@ setInterval(() => {
 }, 10000)
 function createTimestampAndAggregator() {
   const date = new Date();
-  timestamp.push(date.getUTCHours());
-  timestamp.push(date.getUTCMinutes());
-  timestamp.push(date.getUTCSeconds());
-  timestamp.push(date.getUTCMilliseconds());
+  timestamp.push(date.getUTCDate())
+  timestamp.push(date.getUTCMonth()+1)
+  timestamp.push(date.getUTCFullYear())
+  timestamp.push(date.getUTCHours())
+  timestamp.push(date.getUTCMinutes())
+  timestamp.push(date.getUTCSeconds())
+  timestamp.push(date.getUTCMilliseconds())
   timestamp.push(temp)
   return timestamp;
 }
@@ -55,6 +65,22 @@ function checkInternetConnection() {
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 const portName = '/dev/ttyAMA2';
+// const data = [14, 30, 59, 20, 224, 2, 36, 0, 2, 112, 164, 224, 79, 88, 75, 24, 35, 12, 77, 98, 43, 21, 79, 34, 83, 70, 50, 70, 50, 22, 23, 153,15,9,2021, 10, 32, 21, 755, 'dca632b8f170'];
+
+
+// const server = `${url}/api/v1/aggregator/updateAggregatorAndBox`
+// // if(checkInternetConnection()) {
+// //   axios.put(server, {
+// //     body: {
+// //       data: (data)
+// //     },
+  
+// //   }).then(res => {
+// //     console.log(res.data.result.body.data)
+// //   }).catch(e => {
+// //     console.log(e.message);
+// //   })
+// // }
 let sp, sp1;
 sp = new serial(portName, {
   baudRate: 115200,
@@ -63,11 +89,11 @@ const options = {
   length: 32
 }
 sp1 = sp.pipe(new ByteLength(options))
-
+let result;
 
 
 sp1.on('data', (data) => {
-
+ 
   let val = JSON.stringify(data);
   val = (JSON.parse(val));
   const time = createTimestampAndAggregator();
@@ -75,9 +101,10 @@ sp1.on('data', (data) => {
   const internet = checkInternetConnection();
   if (internet) {
     const server = `${url}/api/v1/aggregator/updateAggregatorAndBox`
-    axios.post(server, {
+    axios.put(server, {
       body: {
-        data: JSON.stringify(finaldata)
+        data: (finaldata),
+          
       },
 
     }).then(res => {
